409dfeae01ec4c71f920572050c2fdaa578d9e33
alvarosca <alvaro.sca8@gmail.com>
2024-12-13 16:52:50 -0300

    Report download added

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import io
 import csv
 from PIL import Image
 import numpy as np
+import pandas as pd
 
 overlap = 0.5
 
 # Define label list
-label_list = ['Positive', 'Negative', 'Not Important']
+label_list = ['Positivo', 'Negativo', 'No importante']
 
 # Initialize session state to store points and labels if not already present
 if 'result_dict' not in st.session_state:
@@ -54,22 +55,25 @@ def image_split(image, height=512, width=512, overlap=0):
 
 
 # Image upload
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Subir imagen", type=["jpg", "jpeg", "png"])
 
 # Check if an image is uploaded
 if uploaded_file is not None:
+
+    uploaded_file_name = uploaded_file.name[:-4]
+
     # Open the uploaded image using PIL
     complete_image = Image.open(uploaded_file)
     
     # Display the complete image before any processing
-    st.image(complete_image, caption="Uploaded Image", use_column_width=True)
+    st.image(complete_image, caption="Imagen seleccionada", use_column_width=True)
     
     # Let the user select patch resolution
     col1, col2 = st.columns(2)
-    patch_height = col1.number_input("Select patch height", min_value=16, max_value=complete_image.size[1], value=512, step=16)
-    patch_width = col2.number_input("Select patch width", min_value=16, max_value=complete_image.size[0], value=512, step=16)
+    patch_height = col1.number_input("Seleccionar altura de la sub-imagen", min_value=16, max_value=complete_image.size[1], value=512, step=16)
+    patch_width = col2.number_input("Seleccionar ancho de la sub-imagen", min_value=16, max_value=complete_image.size[0], value=512, step=16)
     
-    if st.button("Generate Patches"):
+    if st.button("Generar sub-imágenes"):
         splits, vert_splits, hor_splits = image_split(np.array(complete_image), height=patch_height, width=patch_width, overlap=overlap)
         st.session_state['vert_splits'] = vert_splits
         st.session_state['hor_splits'] = hor_splits
@@ -89,9 +93,9 @@ if uploaded_file is not None:
     if st.session_state['patches'] is not None:
 
         st.session_state['patch_index'] = st.selectbox(
-            "Select a patch to display", 
+            "Seleccionar sub-imagen a mostrar", 
             options=list(range(len(st.session_state['patches']))), 
-            format_func=lambda x: f"Patch {x + 1}", 
+            format_func=lambda x: f"Sub-imagen {x + 1}", 
             index=st.session_state['patch_index']
         )
 
@@ -103,11 +107,9 @@ if uploaded_file is not None:
             if st.button("Next"):
                 st.session_state['patch_index'] = min(len(st.session_state['patches']) - 1, st.session_state['patch_index'] + 1)
         
-
         img_path = f"patch_{st.session_state['patch_index']}.jpg"
         img = Image.fromarray(st.session_state['patches'][st.session_state['patch_index']])
-               
-        
+                       
         # Use pointdet to annotate the image
         new_labels = pointdet(
             image_path=img_path,
@@ -152,61 +154,64 @@ if uploaded_file is not None:
                 st.session_state['result_dict'][patch_name]['points'].extend(points)
                 st.session_state['result_dict'][patch_name]['labels'].extend(labels)
 
-        if st.button("Finish annotations"):
+        # Process annotations and generate download content
+        if st.button("Terminar anotaciones"):
 
-            all_points = set()  # Set to track unique points
-            all_labels = {}  # Dictionary to track labels for each unique point
-
-            vert_splits = st.session_state['vert_splits']
-            hor_splits = st.session_state['hor_splits']
-            overlap = st.session_state['overlap']
+            all_points = []
+            all_labels = []
 
             for patch_name, data in st.session_state['result_dict'].items():
                 patch_points = data['points']
                 patch_labels = data['labels']
 
-                patch_index = data['patch_index']
-                patch_x_coord = (patch_index % hor_splits) * (patch_width * (1 - overlap))
-                patch_y_coord = (patch_index // vert_splits) * (patch_height * (1 - overlap))
-
                 for point, label in zip(patch_points, patch_labels):
                     x, y = point
-
-                    x += patch_x_coord
-                    y += patch_y_coord
-
                     x = int(x)
                     y = int(y)
+                    all_points.append([x, y])
+                    all_labels.append(label)
 
-                    point_tuple = (x, y)
-                    
-                    # Only add if it's not a duplicate point
-                    if point_tuple not in all_points:
-                        all_points.add(point_tuple)
-                        all_labels[point_tuple] = label  # Store the label for this point
-                    else:
-                        # Optional: Update the label if you want the latest label to be stored
-                        all_labels[point_tuple] = label
+            # Create a DataFrame to store points and labels
+            df = pd.DataFrame(all_points, columns=["X", "Y"])
+            df["Label"] = all_labels
 
-            # If you need lists for all_points and all_labels
-            all_points = list(all_points)
-            all_labels = [all_labels[point] for point in all_points]
-
-
-            # Create CSV content
+            # Create CSV content for download
             csv_buffer = io.StringIO()
-            csv_writer = csv.writer(csv_buffer)
-            csv_writer.writerow(["X", "Y", "Label"])
-            for point, label in zip(all_points, all_labels):
-                csv_writer.writerow([point[0], point[1], label_list[label]])
+            df.to_csv(csv_buffer, index=False)
+            csv_data = csv_buffer.getvalue()
 
-            # Convert CSV buffer to downloadable file
-            csv_data = csv_buffer.getvalue().encode('utf-8')
-            st.download_button(
-                label="Download CSV",
-                data=csv_data,
-                file_name="annotations.csv",
-                mime="text/csv"
-            )
-else:
-    st.write("Please upload an image.")
+            # **Generate the Annotation Report**
+            num_positive = all_labels.count('Positivo')
+            num_negative = all_labels.count('Negativo')
+
+            report_content = f"""
+            Reporte de anotación
+            ==================
+            Nombre de la imagen: {uploaded_file_name}
+            Número de puntos positivos: {label_list[0]}
+            Número de puntos negativos: {label_list[1]}
+            """
+
+            # Create file-like object to download the report
+            report_buffer = io.StringIO()
+            report_buffer.write(report_content)
+            report_data = report_buffer.getvalue()
+
+            col1, col2 = st.columns([6, 6])
+            with col1:
+                # **1st Download Button** - CSV of all points and labels
+                st.download_button(
+                    label="Descargar Anotaciones (CSV)",
+                    data=csv_data,
+                    file_name=f'{uploaded_file_name}.csv',
+                    mime='text/csv'
+                )
+
+            with col2:
+                # **2nd Download Button** - Annotation Report
+                st.download_button(
+                    label="Descargar Reporte (txt)",
+                    data=report_data,
+                    file_name=f'{uploaded_file_name}_reporte.txt',
+                    mime='text/plain'
+                )
